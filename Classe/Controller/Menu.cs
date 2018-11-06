6dd3980a805c69d15d49d61de8d14371f2ccba94
Franklin Bitencourt <Chingling152@hotmail.com>
2018-11-06 17:19:02 -0200

    V 1.3 + Adicionado Menu.MostrarExtrato e Menu.EfetuarTransacao + Adicionado Menu.ExportarDataBase e Menu.MostrarRelatorio + Adicionado Menu.MostrarTodos e Menu.MostrarTransacao

--- Classe/Controller/Menu.cs
+++ Classe/Controller/Menu.cs
@@ -3,6 +3,9 @@ namespace FinancaDeMesa.Classe.Controller
 {
     public static class Menu
     {
+        private static bool Vazia(){
+            return false;
+        }
         #region Metodos deslogado
         /// <summary>
         /// Menu onde o usuario terá que inserir um dos valores abaixo :  
@@ -25,13 +28,48 @@ namespace FinancaDeMesa.Classe.Controller
         #region Metodos logado
         /// <summary>
         /// Menu onde o usuario deve inserir um dos valores abaixo :  
-        /// 1 - Efetuar Transação   
-        /// 2 - 
+        /// 1 - Efetuar Transação     
+        /// 2 - Mostrar Extrato   
+        /// 3 - Mostrar Relatorio   
+        /// 4 - Fazer logoff (MenuDeslogado)  
         /// </summary>
         public static void Logado(){
 
         }
+        /// <summary>
+        /// Recebe dados do usuario e cria uma transação e salva elas no ID do usuario  
+        /// </summary>
         private static void EfetuarTransacao(){
+
+        }
+        /// <summary>
+        /// Mostra todas as transações feitas pelo usuario
+        /// </summary>
+        private static void MostrarExtrato(){
+
+        }
+        /// <summary>
+        /// Salva o banco de dados em um arquivo .zip
+        /// </summary>
+        private static void ExportarDataBase(){
+
+        }
+
+        #endregion
+
+        #region Relatorios
+        /// <summary>
+        /// Abre um menu onde o usuario deve escolher entre gerar um arquivo .docx contendo : 
+        /// 1 - Somente suas transações
+        /// 2 - Lista de todos os usuarios 
+        /// </summary>
+        private static void MostrarRelatorio(){
+
+        }
+        private static void MostrarTodos(){
+
+        }
+        private static void MostrarTransacao(){
 
         }
         #endregion
