2cbb4fb8f0a223a64c56ee610ef9ffbb5593d654
Aline Ibrahin Chahine <35482820831@info.local>
2018-11-06 11:17:10 -0200

    Menu cadastro

--- Classe/Controller/Menu.cs
+++ Classe/Controller/Menu.cs
@@ -1,8 +1,37 @@
 using System;
+using System.Collections.Generic;
+
 namespace FinancaDeMesa.Classe.Controller
 {
     public static class Menu
     {
-        
-    }
+        /// <summary>
+        /// Método que mostra a tela de cadastro do usuário, e pede nome, e-mail e senha
+        /// </summary>
+        public static void CadastrarUsuario(){
+            string nome, email, senha;
+
+            // Inserindo o nome do usuário
+            do
+            {
+                System.Console.WriteLine("Digite o seu nome");
+                nome = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(nome))
+                {
+                    Console.WriteLine("Nome inválido");
+                }
+            }
+            while (string.IsNullOrEmpty(nome));
+
+            // Inserindo o e-mail
+            do
+            {
+                Console.WriteLine("Digite o seu endereço de e-mail (deve conter '@' e '.");
+                email = Console.ReadLine();
+
+            }
+            while (string.IsNullOrEmpty(email));
+        }
+    }   
 }
