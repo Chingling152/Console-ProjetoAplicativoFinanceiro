e3f7001155f13d69be975782e2c0caa70759f779
Franklin Bitencourt <Chingling152@hotmail.com>
2018-11-06 16:50:51 -0200

    V 1.2 + Adicionado Menu.Logado , Menu.Deslogado , Menu.Cadastrar e Menu.Logar * Adicionado UTF-8 em Design.cs - Fixado erro na validação de valor da transação

--- Classe/Controller/Menu.cs
+++ Classe/Controller/Menu.cs
@@ -3,6 +3,37 @@ namespace FinancaDeMesa.Classe.Controller
 {
     public static class Menu
     {
-        
+        #region Metodos deslogado
+        /// <summary>
+        /// Menu onde o usuario terá que inserir um dos valores abaixo :  
+        /// 1 - Cadastrar-se (Metodo Cadastrar)  
+        /// 2 - Logar (Metodo Logar)     
+        /// 3 - Sair (Ignora o do while)  
+        /// Qualquer outro valor sera ignorado  
+        /// </summary>
+        public static void Deslogado(){
+
+        }
+        private static void Cadastrar(){
+
+        }
+        private static void Logar(){
+
+        }
+        #endregion
+
+        #region Metodos logado
+        /// <summary>
+        /// Menu onde o usuario deve inserir um dos valores abaixo :  
+        /// 1 - Efetuar Transação   
+        /// 2 - 
+        /// </summary>
+        public static void Logado(){
+
+        }
+        private static void EfetuarTransacao(){
+
+        }
+        #endregion
     }
 }
--- Classe/Models/Transacao.cs
+++ Classe/Models/Transacao.cs
@@ -1,7 +1,72 @@
-namespace FinancaDeMesa.Classe.Models
-{
-    public class Transacao
-    {
-        
+using System;
+using FinancaDeMesa.Classe.Util;
+
+namespace FinancaDeMesa.Classe.Models {
+
+    public enum tipoTransacao {
+        Despesa = 1,
+        Receita = 2,
+        Invalido = 0
     }
+    public class Transacao {
+        /// <summary>
+        /// Id da transacao
+        /// </summary>
+        public int ID;
+        /// <summary>
+        /// Define o tipo de transação que será feita
+        /// </summary>
+        public tipoTransacao tipo;
+        /// <summary>
+        /// Define a descrição da transação
+        /// </summary>
+        public string Descricao;
+        /// <summary>
+        /// Representa a qual usuario essa transação pertence
+        /// </summary>
+        public int IDUsuario;
+
+        private string dataTransacao;
+        private double ValorDespesa;
+
+        /// <summary>
+        /// Converte o valor inserido pelo usuario
+        /// </summary>
+        /// <param name="valor">Qualquer valor numerico</param>
+        public void ValidarValor (double valor) {
+            switch (tipo) {
+                case tipoTransacao.Despesa:
+                    if(valor >= 0){
+                        ValorDespesa = valor * -1;
+                    }else{
+                        ValorDespesa = valor;
+                    }
+                    break;
+                case tipoTransacao.Receita:
+                    if(valor >= 0){
+                        ValorDespesa = valor;
+                    }else{
+                        ValorDespesa = valor * (- 1);
+                    }
+                    break;
+                default:
+                    Design.MensagemErro("Isso não devia acontecer ;-; não sei como você fez isso mas ...  saia já");
+                    break;
+            }
+        }
+        /// <summary>
+        /// Valida a data inseirda pelo usuario e transforma ela em uma string 
+        /// </summary>
+        /// <param name="data">Qualquer valor de data</param>
+        public void ValidarData(System.DateTime data){
+            if(data.Year >= DateTime.Now.Year ){
+                if(data.Month >= DateTime.Now.Month ){
+                    if(data.Day > DateTime.Now.Day){
+                        data = DateTime.Now;
+                    }
+                }
+            }
+            dataTransacao =data.ToShortDateString();
+        }
+    } 
 }
