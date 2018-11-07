26870597597f6291353889bc084ac2f0a2cb4382
Aline Ibrahin Chahine <35482820831@info.local>
2018-11-07 11:32:41 -0200

    Iniciando a parte de Word

--- Classe/Models/Relatorio.cs
+++ Classe/Models/Relatorio.cs
@@ -1,12 +1,47 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Drawing;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
 
 namespace FinancaDeMesa.Classe.Models
 {
-    public class Relatorio
+    class Relatorio
     {
+        static void Main(string[] args)
+        {
+            #region Criando o formulário
+            // Criando o formulário
+            Relatorio relatorio = new Relatorio();
+            #endregion
+
+            #region Adicionando uma seção ao relatório
+            //Adicionando uma seção ao relatório
+            Section secao = relatorio.AddSection();
+            #endregion
+
+            #region Adicionando um parágrafo à seção
+            //Adicionando um parágrafo à seção
+            Paragraph paragrafo = secao.AddParagraph();
+            #endregion
+
+            #region Salvando o relatório
+            //Salvando o relatório
+            relatorio.SaveToFile(@"C:\Users\35482820831\Desktop\relatorio.docx",FileFormat.Docx);
+            #endregion
+        }
+
+        //??
+        private void SaveToFile(string v, FileFormat docx)
+        {
+            throw new NotImplementedException();
+        }
         
+        //???
+        private Section AddSection()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
