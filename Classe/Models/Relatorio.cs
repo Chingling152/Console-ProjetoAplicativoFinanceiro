d768a5c8bac8992d11aec393b929116198a132c4
Aline Ibrahin Chahine <35482820831@info.local>
2018-11-07 09:00:26 -0200

    Merge

--- Classe/Models/Relatorio.cs
+++ Classe/Models/Relatorio.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
 namespace FinancaDeMesa.Classe.Models
 {
     public class Relatorio
     {
-        
+        static void Main(string[] args)
+        {
+            //Criar o relatório
+            Relatorio relatorio = new Relatorio();
+
+            //Criar uma seção
+            
+        }
     }
 }
